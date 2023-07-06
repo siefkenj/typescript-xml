26321c172076d0fb0c5df3c6d91958178c75f2c8
Jason Siefken <siefkenj@gmail.com>
2023-07-06 17:30:58 -0400

    Print comments in `rawPrint`

--- packages/relax-ng/src/printer/rnc/print-raw.ts
+++ packages/relax-ng/src/printer/rnc/print-raw.ts
@@ -17,6 +17,7 @@ type Node =
     | DocumentationLine
     | AnnotationAttribute
     | Param;
+type Comment = TopLevel["comments"][number];
 
 /**
  * Whether the content of `node` needs wrapping in parenthesis when an operation is applied to it.
@@ -35,11 +36,34 @@ function needsParentheses(node: Node): boolean {
     return false;
 }
 
-
 /**
- * Print the RNC AST to a string with as little formatting as possible.
+ * Print the RNC AST to a string with as little formatting as possible. If `comments` is provided
+ * and the nodes have `position` information, comments will be inserted. The supplied `comments` array will be mutated.
  */
-export function printRaw(node: Node | string): string {
+export function printRaw(node: Node | string, comments?: Comment[]): string {
+    const printRawBound = (node: Node | string) => {
+        // Find all comments that are before the current node
+        let leadingComments: string[] = [];
+        if (
+            Array.isArray(comments) &&
+            comments.length > 0 &&
+            comments[0].position &&
+            typeof node !== "string"
+        ) {
+            const nodePosition = node.position;
+            const commentsBefore: Comment[] = [];
+            while (
+                comments.length > 0 &&
+                comments[0].position.end.offset <= nodePosition.start.offset
+            ) {
+                commentsBefore.push(comments.shift()!);
+            }
+            leadingComments = commentsBefore.map((x) => `#${x.content}\n`);
+        }
+        let printedNodes = printRaw(node, comments);
+        return `${leadingComments.join("")}${printedNodes}`;
+    };
+
     if (typeof node === "string") {
         return node;
     }
@@ -47,9 +71,9 @@ export function printRaw(node: Node | string): string {
     switch (type) {
         case "annotated": {
             let annotations = "";
-            const content = printRaw(node.content);
+            const content = printRawBound(node.content);
             if (node.annotations.length > 0) {
-                annotations = node.annotations.map(printRaw).join("\n");
+                annotations = node.annotations.map(printRawBound).join("\n");
             }
             if (annotations.length > 0 && !annotations.endsWith("\n")) {
                 annotations += "\n";
@@ -60,12 +84,13 @@ export function printRaw(node: Node | string): string {
                 node.followAnnotations.length > 0
             ) {
                 followAnnotations =
-                    " >> " + node.followAnnotations.map(printRaw).join(" >> ");
+                    " >> " +
+                    node.followAnnotations.map(printRawBound).join(" >> ");
             }
             return `${annotations}${content}${followAnnotations}`;
         }
         case "annotation": {
-            const content = node.content.map(printRaw).join(" ");
+            const content = node.content.map(printRawBound).join(" ");
             if (content.length === 0) {
                 return "[]";
             }
@@ -74,10 +99,10 @@ export function printRaw(node: Node | string): string {
         case "param":
         // Fallthrough on purpose
         case "annotationAttribute": {
-            return `${node.name}=${printRaw(node.value)}`;
+            return `${node.name}=${printRawBound(node.value)}`;
         }
         case "annotationElement": {
-            let content = node.content.map(printRaw).join(" ");
+            let content = node.content.map(printRawBound).join(" ");
             if (content.length === 0) {
                 content = "[]";
             } else {
@@ -87,19 +112,19 @@ export function printRaw(node: Node | string): string {
             return `${node.name}${content}`;
         }
         case "attribute": {
-            const name = printRaw(node.name);
-            let content = printRaw(node.content);
+            const name = printRawBound(node.name);
+            let content = printRawBound(node.content);
             if (content.length > 0) {
                 content = ` ${content} `;
             }
             return `attribute ${name} {${content}}`;
         }
         case "choice": {
-            const content = node.content.map(printRaw).join(" | ");
+            const content = node.content.map(printRawBound).join(" | ");
             return content;
         }
         case "datatype": {
-            const content = printRaw(node.content);
+            const content = printRawBound(node.content);
             if (node.name) {
                 return `${node.name} ${content}`;
             }
@@ -108,11 +133,11 @@ export function printRaw(node: Node | string): string {
         case "datatypeExcept": {
             let params = "";
             if (node.params.length > 0) {
-                params = ` { ${node.params.map(printRaw).join(" ")} }`;
+                params = ` { ${node.params.map(printRawBound).join(" ")} }`;
             }
             let except = "";
             if (node.except) {
-                let pattern = printRaw(node.except.content);
+                let pattern = printRawBound(node.except.content);
                 if (needsParentheses(node.except.content)) {
                     pattern = `(${pattern})`;
                 }
@@ -121,7 +146,7 @@ export function printRaw(node: Node | string): string {
             return `${node.name}${params}${except}`;
         }
         case "declaration": {
-            const content = printRaw(node.content);
+            const content = printRawBound(node.content);
             const name = node.name ? node.name + " " : "";
             switch (node.declarationType) {
                 case "datatypes":
@@ -133,19 +158,19 @@ export function printRaw(node: Node | string): string {
             }
         }
         case "define": {
-            const content = printRaw(node.content);
+            const content = printRawBound(node.content);
             return `${node.name} = ${content}`;
         }
         case "div": {
-            const content = node.content.map(printRaw).join("\n");
+            const content = node.content.map(printRawBound).join("\n");
             return `div { ${content} }`;
         }
         case "documentationLine": {
             return `## ${node.content}`;
         }
         case "element": {
-            const name = printRaw(node.name);
-            let content = printRaw(node.content);
+            const name = printRawBound(node.name);
+            let content = printRawBound(node.content);
             if (content.length > 0) {
                 content = ` ${content} `;
             }
@@ -155,23 +180,23 @@ export function printRaw(node: Node | string): string {
             return "empty";
         }
         case "external": {
-            const content = printRaw(node.content);
+            const content = printRawBound(node.content);
             if (node.inherit != null) {
                 return `external ${content} inherit = ${node.inherit}`;
             }
             return `external ${content}`;
         }
         case "grammar": {
-            const content = node.content.map(printRaw).join("\n");
+            const content = node.content.map(printRawBound).join("\n");
             return `grammar { ${content} }`;
         }
         case "group": {
-            const content = node.content.map(printRaw).join(", ");
+            const content = node.content.map(printRawBound).join(", ");
             return content;
         }
         case "include": {
-            const content = node.content.map(printRaw).join("\n");
-            const uri = printRaw(node.uri);
+            const content = node.content.map(printRawBound).join("\n");
+            const uri = printRawBound(node.uri);
             const inherit =
                 node.inherit != null ? ` inherit = ${node.inherit}` : "";
             if (content.length === 0) {
@@ -180,15 +205,15 @@ export function printRaw(node: Node | string): string {
             return `include ${uri}${inherit} { ${content} }`;
         }
         case "interleave": {
-            const content = node.content.map(printRaw).join(" & ");
+            const content = node.content.map(printRawBound).join(" & ");
             return content;
         }
         case "list": {
-            const content = printRaw(node.content);
+            const content = printRawBound(node.content);
             return `list { ${content} }`;
         }
         case "mixed": {
-            let content = printRaw(node.content);
+            let content = printRawBound(node.content);
             if (content.length > 0) {
                 content = ` ${content} `;
             }
@@ -203,24 +228,34 @@ export function printRaw(node: Node | string): string {
             return node.name;
         }
         case "repeat": {
-            const content = printRaw(node.content);
+            const content = printRawBound(node.content);
             if (needsParentheses(node.content)) {
                 return `(${content})${node.operation}`;
             }
             return `${content}${node.operation}`;
         }
         case "root": {
-            return node.content.map(printRaw).join("\n");
+            // The comments array will be mutated, so we make a copy
+            comments = [...node.comments];
+            // `printRawBound` already references the `comments` variable like it's a global,
+            // so we don't need to pass it in and instead can use `printRawBound` directly.
+            let ret = node.content.map(printRawBound).join("\n");
+            // By now all comments that occur before a node have been printed, but we
+            // need to handle comments that occur after the last node.
+            let trailingComments = comments
+                .map((x) => `\n#${x.content}`)
+                .join("");
+            return ret + trailingComments;
         }
         case "start": {
-            const content = printRaw(node.content);
+            const content = printRawBound(node.content);
             return `start ${node.operation} ${content}`;
         }
         case "text": {
             return "text";
         }
         case "nameclassList": {
-            return node.content.map(printRaw).join(" | ");
+            return node.content.map(printRawBound).join(" | ");
         }
         case "nameclass": {
             return node.name;
@@ -229,14 +264,14 @@ export function printRaw(node: Node | string): string {
             if (!node.except) {
                 return node.name;
             }
-            let except = printRaw(node.except);
+            let except = printRawBound(node.except);
             if (needsParentheses(node.except)) {
                 except = `(${except})`;
             }
             return `${node.name} - ${except}`;
         }
         case "literal": {
-            return node.parts.map(printRaw).join(" ~ ");
+            return node.parts.map(printRawBound).join(" ~ ");
         }
         case "literalSegment": {
             return `${node.escape}${node.value}${node.escape}`;
